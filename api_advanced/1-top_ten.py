6e2fba46edff1c775bcdec8938c0fd4ddfac0578
MKangabire <m.kangabire@alustudent.com>
2023-11-09 02:13:38 -0800

    API Advance

---
+++ api_advanced/1-top_ten.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+a function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.
+"""
+
+import json
+import requests
+
+def top_ten(subreddit):
+    """
+    prints the titles of the first 10 hot posts
+    """
+    URL = "https://www.reddit.com/r/{}/top.json".format(subreddit)
+
+    headers = {"User-Agent": "0-subs/2.0"}
+
+    response = requests.get(URL, headers=headers)
+
+    if (response.status_code) == 200:
+        json_response = response.json()
+
+        sorted_posts = sorted(json_response['data']['children'], key=lambda x: x['data']['score'], reverse=True)
+
+
+        for post in sorted_posts[:10]:
+            title = post['data']['title']
+            print(title)
+
+        else:
+            print("error")
