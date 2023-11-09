41386e7570d05b0860ac3b010fce4121f5fbef2d
MKangabire <m.kangabire@alustudent.com>
2023-11-09 06:38:32 -0800

    API Advance

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """
-prints OK if ran successful.
+prints the titles of
+the first 10 hot posts listed
+for a given subreddit.
 """
+
 
 import json
 import requests
@@ -10,27 +13,22 @@ import sys
 
 def top_ten(subreddit):
     """
-    prints the titles of the first 10 hot posts
+    prints 10 hot posts
     """
-    URL = "https://www.reddit.com/r/{}/top.json".format(subreddit)
-
-    params = {
-            "t": "all",
-            "count": 0,
-            "limit": 10
+    URL = "https://www.reddit.com/r/{}/Hot.json".format(subreddit)
+    headers = {
+            "User-Agent": "1-top_ten/1.0"
     }
 
-    headers = {"User-Agent": "0-subs/2.0"}
+    response = requests.get(URL, headers=headers)
 
-    response = requests.get(URL, params=params, headers=headers)
-
-    if response.status_code == 200:
-        resp_json = response.json()
-        resp_children = resp_json['data']['top_ten']
-        return resp_children 
+    if (response.status_code) == 200:
+        json_response = response.json()
+        titles = json_response['data']['top_ten']
+        return titles
 
     else:
-        print'(None')
+        print('None')
 
-if __main__ == "__main__":
+if __name__ == '__main__':
     pass
